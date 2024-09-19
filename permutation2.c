cbe82d2ac45a380780b960095c2e15a54a1fcd36
Chihen-Tai <microdaery@gapp.nthu.edu.tw>
2024-09-20 02:27:21 +0800

    homework

---
+++ permutation2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int n,k;
+int a[2000005];
+
+void permutation(int len,int start)
+{
+    if(len==k+1)
+    {
+        for(int i=1;i<=k;i++)
+        {
+            printf("%d ",a[i]);
+        }
+        printf("\n");
+        return ;
+    }
+    else
+    {
+        for(int i=start;i<=n;i++)
+        {
+            a[len]=i;
+            permutation(len+1,i+1);
+        }
+    }
+}
+
+int main()
+{
+    n=5,k=3;
+    permutation(1,1);
+}
